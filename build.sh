5d037a94127ab849719de3ed8ed5afb3871cf40e
ghazzor <kartikhis8o@gmail.com>
2025-11-06 14:43:23 +0530

    omen16: disable some intell stuff explicitly & enable lto explicitly

--- build.sh
+++ build.sh
@@ -26,9 +26,6 @@ cp Makefile.custom kernel/xanmod/
 cd kernel/xanmod
 sed -i 's/KBUILD_CFLAGS += -O2/include \$(srctree)\/Makefile.custom/g' Makefile
 echo 'source "Kconfig.custom"' >> Kconfig
-# disable -O3 from arch/x86
-sed -i 's/-O3/-O2/g' arch/x86/Makefile
-sed -i 's/-Copt-level=3/-Copt-level=2/g' arch/x86/Makefile
 
 if [ "$1" = "-d" ]; then
 	echo "dirty"
